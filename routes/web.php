c904b20458b37db1fb75649cca56436dcae54fb0
Ratih Murningsih <ratihmurningsih@gmail.com>
2020-12-19 12:09:43 +0700

    membuat presensi masuk, done

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,26 @@
-new<?php
+<?php
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PresensiController;
 
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-route::get('/registrasi', [LoginController::class,'registrasi'])->name('registrasi');
-route::post('/simpanregistrasi', [LoginController::class,'simpanregistrasi'])->name('simpanregistrasi');
-route::get('/login', [LoginController::class,'halamanlogin'])->name('login');
-route::post('/postlogin', [LoginController::class,'postlogin'])->name('postlogin');
-route::get('/logout', [LoginController::class,'logout'])->name('logout');
+route::get('/registrasi',[LoginController::class,'registrasi'])->name('registrasi');
+route::post('/simpanregistrasi',[LoginController::class,'simpanregistrasi'])->name('simpanregistrasi');
+route::get('/login',[LoginController::class,'halamanlogin'])->name('login');
+route::post('/postlogin',[LoginController::class,'postlogin'])->name('postlogin');
+route::get('/logout',[LoginController::class,'logout'])->name('logout');
 
 Route::group(['middleware' => ['auth','ceklevel:admin,karyawan']], function () {
     route::get('/home', [HomeController::class,'index'])->name('home');
+});
+
+Route::group(['middleware' => ['auth','ceklevel:karyawan']], function () {
+    route::post('/simpan-masuk', [PresensiController::class,'store'])->name('simpan-masuk');
+    route::get('/presensi-masuk', [PresensiController::class,'index'])->name('presensi-masuk');
 });
